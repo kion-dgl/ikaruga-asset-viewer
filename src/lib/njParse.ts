67613487b17b82722504e18b7ce667fb19a04e3c
kion <kion@dashgl.com>
2025-03-15 22:41:51 -0500

    implement njtl

--- src/lib/njParse.ts
+++ src/lib/njParse.ts
@@ -1,3 +1,31 @@
-import * as THREE from "three";
+// import * as THREE from "three";
+import ByteReader from "ByteReader";
 
-export const parseNinjaModel = (buffer: ArrayBuffer) => {};
+const parseNinjaModel = (buffer: ArrayBuffer) => {
+  const reader = new ByteReader(buffer);
+  const firstMagic = reader.readString(4);
+
+  if (firstMagic === "NJTL") {
+    // Get the length of the NJTL chunk
+    const len = reader.readInt32();
+    const ref = reader.tell();
+    const ptr = reader.readUInt32();
+    const count = reader.readUInt32();
+    reader.seek(ptr + ref);
+
+    const textureNamePointers: number[] = [];
+    for (let i = 0; i < count; i++) {
+      textureNamePointers.push(reader.readUInt32());
+      reader.seekRel(8);
+    }
+
+    textureNamePointers.forEach((ptr) => {
+      reader.seek(ref + ptr);
+      const string = reader.readString();
+      console.log(string);
+    });
+  }
+  console.log(firstMagic);
+};
+
+export { parseNinjaModel };
